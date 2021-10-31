dce459b74807fd131eb6296e033fc4fb19699f9b
Aditi <aditisneh.27@gmail.com>
2021-10-31 10:06:51 +0530

    init

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import AddItems from "./AddItems";
-import configureStore from "./store/configStore";
+import configureStore from "./store/configStore.js";
 
 const store = configureStore();
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,8 +1,5 @@
 import { combineReducers } from "redux";
 import entitiesReducers from "./entities";
-import { createBrowserHistory } from "history";
-
-export const history = createBrowserHistory();
 
 export default combineReducers({
   entities: entitiesReducers,
